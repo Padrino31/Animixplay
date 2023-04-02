fbea9e63b4f0314ed53ba7eb0076825aa62cc00a
Padrino31 <92399279+Padrino31@users.noreply.github.com>
2023-04-01 18:44:08 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 from os import getenv
 
 
-API_KEY = "Paste Your Key Here"
+API_KEY = "TYEMQI"
 
 if not API_KEY or API_KEY == "Paste Your Key Here" or API_KEY == "":
-    API_KEY = getenv("API_KEY")
+    API_KEY = getenv("TYEMQI")
 
     if not API_KEY:
         raise Exception("Please add your TechZ Api Key in config.py file")
