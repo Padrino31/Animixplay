6f84ef0c9c8d3232bf91951c1e0f40a02b740806
Padrino31 <92399279+Padrino31@users.noreply.github.com>
2023-04-06 09:15:50 -0600

    Update serviceworker.js

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -1,24 +1,12 @@
-var cacheName = 'Animeflv';
-var filesToCache = [
-  '/',
-  '/static/css/styles.css',
-  '/static/js/app.js',
-  '/static/images/logo.png',
-  'https://od.lk/s/OTBfMzU4MDc3MjNf/ApplogoDark.png'
-];
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+self.__WB_DISABLE_DEV_LOGS = true;
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+// eslint-disable-next-line no-undef
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  // eslint-disable-next-line no-undef
+  new workbox.strategies.NetworkFirst()
+);
