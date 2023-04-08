34fb951847570cc32d22a80b4ac8ce46056025fc
Padrino31 <92399279+Padrino31@users.noreply.github.com>
2023-04-08 10:15:44 -0600

    Update script.js

--- static/css/script.js
+++ static/css/script.js
@@ -3,73 +3,59 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyAn7QiOmZcOkdCXS9Ugp0S6gGMx7x-cDIk",
-
-    authDomain: "login-app-695a4.firebaseapp.com",
-
-    projectId: "login-app-695a4",
-
-    storageBucket: "login-app-695a4.appspot.com",
-
-    messagingSenderId: "710961899998",
-
-    appId: "1:710961899998:web:7f79123b6e67129bc8154f"
-
-  };
-
+  apiKey: "AIzaSyAn7QiOmZcOkdCXS9Ugp0S6gGMx7x-cDIk",
+  authDomain: "login-app-695a4.firebaseapp.com",
+  projectId: "login-app-695a4",
+  storageBucket: "login-app-695a4.appspot.com",
+  messagingSenderId: "710961899998",
+  appId: "1:710961899998:web:7f79123b6e67129bc8154f"
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-document.getElementById("loginForm").addEventListener("submit",(event)=>{
-    event.preventDefault()
-})
+document.getElementById("loginForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
 
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        location.replace("welcome.html")
-    }
-})
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    location.replace("welcome.html");
+  } else {
+    location.replace("index.html");
+  }
+});
 
-function login(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch((error)=>{
-        document.getElementById("error").innerHTML = error.message
-    })
-}
-
-function signUp(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  signInWithEmailAndPassword(auth, email, password)
     .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
+      document.getElementById("error").innerHTML = error.message;
     });
 }
 
-function forgotPass(){
-    const email = document.getElementById("email").value
-    firebase.auth().sendPasswordResetEmail(email)
+function signUp() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .catch((error) => {
+      document.getElementById("error").innerHTML = error.message;
+    });
+}
+
+function forgotPass() {
+  const email = document.getElementById("email").value;
+  sendPasswordResetEmail(auth, email)
     .then(() => {
-        alert("Reset link sent to your email id")
+      alert("Reset link sent to your email id");
     })
     .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
+      document.getElementById("error").innerHTML = error.message;
     });
 }
-firebase.auth().onAuthStateChanged((user)=>{
-    if(!user){
-        location.replace("index.html")
-    }else{
-        document.getElementById("user").innerHTML = "Hello, "+user.email
-    }
-})
 
-
-function logout(){
-    firebase.auth().signOut()
+function logout() {
+  auth.signOut();
 }
