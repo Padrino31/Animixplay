da89ef6161b39330963c6817d0c6ee2780cf0d61
Padrino31 <92399279+Padrino31@users.noreply.github.com>
2023-04-08 16:13:59 -0600

    Update script.js

--- static/css/script.js
+++ static/css/script.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAn7QiOmZcOkdCXS9Ugp0S6gGMx7x-cDIk",
@@ -32,74 +32,111 @@ const createacctbtn = document.getElementById("create-acct-btn");
 
 const returnBtn = document.getElementById("return-btn");
 
-var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
+let email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
+let userId, watchlist, complete, bookmark;
+
+// Retrieve data from local storage
+watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+complete = JSON.parse(localStorage.getItem("complete")) || [];
+bookmark = JSON.parse(localStorage.getItem("bookmark")) || [];
+
+// Update local storage
+const updateLocalStorage = () => {
+  localStorage.setItem("watchlist", JSON.stringify(watchlist));
+  localStorage.setItem("complete", JSON.stringify(complete));
+  localStorage.setItem("bookmark", JSON.stringify(bookmark));
+};
 
 createacctbtn.addEventListener("click", function() {
-  var isVerified = true;
+  let isVerified = true;
 
   signupEmail = signupEmailIn.value;
   confirmSignupEmail = confirmSignupEmailIn.value;
-  if(signupEmail != confirmSignupEmail) {
-      window.alert("Email fields do not match. Try again.")
-      isVerified = false;
+  if (signupEmail != confirmSignupEmail) {
+    window.alert("Email fields do not match. Try again.");
+    isVerified = false;
   }
 
   signupPassword = signupPasswordIn.value;
   confirmSignUpPassword = confirmSignUpPasswordIn.value;
-  if(signupPassword != confirmSignUpPassword) {
-      window.alert("Password fields do not match. Try again.")
-      isVerified = false;
+  if (signupPassword != confirmSignUpPassword) {
+    window.alert("Password fields do not match. Try again.");
+    isVerified = false;
   }
-  
-  if(signupEmail == null || confirmSignupEmail == null || signupPassword == null || confirmSignUpPassword == null) {
+
+  if (signupEmail == null || confirmSignupEmail == null || signupPassword == null || confirmSignUpPassword == null) {
     window.alert("Please fill out all required fields.");
     isVerified = false;
   }
-  
-  if(isVerified) {
+
+  if (isVerified) {
     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
       .then((userCredential) => {
         const user = userCredential.user;
-        const userId = user.uid;
-        set(ref(database, 'users/' + userId), {
-          email: signupEmail,
-          watchlist: [],
-          completed: [],
-          bookmarks: []
-        }).then(() => {
-          window.alert("Success! Account created.");
-          window.location.href = "./login.html";
-        });
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        window.alert("Error occurred. Try again.");
-      });
-  }
-});
-submitButton.addEventListener("click", function() {
-  email = emailInput.value;
-  password = passwordInput.value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log("Success! Welcome back!");
-      window.location.href = "/";
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      window.alert("Error occurred. Try again.");
+        userId = user.uid; // set userId to the UID of the newly created user
+        main.style.display = "block"; // display the main content
+        createacct.style.display = "none"; // hide the create account form
+            // Create a new user in the database with their email as the key
+    set(ref(database, "users/" + userId), {
+      email: signupEmail,
+      watchlist: watchlist,
+      complete: complete,
+      bookmark: bookmark
     });
+
+    // Update local storage to reflect new user
+    updateLocalStorage();
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    window.alert(errorMessage);
+  });
+}
 });
+
+// Function to sign in a user with their email and password
+const signInUser = () => {
+signInWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+const user = userCredential.user;
+userId = user.uid; // set userId to the UID of the signed in user
+setUserData(); // retrieve user data from the database and update local storage
+main.style.display = "block"; // display the main content
+login.style.display = "none"; // hide the login form
+})
+.catch((error) => {
+const errorCode = error.code;
+const errorMessage = error.message;
+window.alert(errorMessage);
+});
+};
+
+submitButton.addEventListener("click", function() {
+email = emailInput.value;
+password = passwordInput.value;
+signInUser();
+});
+
 signupButton.addEventListener("click", function() {
-    main.style.display = "none";
-    createacct.style.display = "block";
+main.style.display = "none"; // hide the main content
+createacct.style.display = "block"; // display the create account form
 });
 
 returnBtn.addEventListener("click", function() {
-    main.style.display = "block";
-    createacct.style.display = "none";
+main.style.display = "none"; // hide the main content
+createacct.style.display = "none"; // hide the create account form
+login.style.display = "block"; // display the login form
 });
+
+// Function to retrieve user data from the database and update local storage
+const setUserData = () => {
+const userRef = ref(database, "users/" + userId);
+userRef.on("value", (snapshot) => {
+const data = snapshot.val();
+watchlist = data.watchlist;
+complete = data.complete;
+bookmark = data.bookmark;
+updateLocalStorage();
+});
+};
