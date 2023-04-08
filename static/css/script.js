ff06fb2315442d12028a5d1bc2682f28dcc456d5
Padrino31 <92399279+Padrino31@users.noreply.github.com>
2023-04-08 15:01:13 -0600

    Update script.js

--- static/css/script.js
+++ static/css/script.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 
-
- const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAn7QiOmZcOkdCXS9Ugp0S6gGMx7x-cDIk",
     authDomain: "login-app-695a4.firebaseapp.com",
     projectId: "login-app-695a4",
     storageBucket: "login-app-695a4.appspot.com",
     messagingSenderId: "710961899998",
     appId: "1:710961899998:web:7f79123b6e67129bc8154f"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
@@ -34,72 +33,6 @@ const createacctbtn = document.getElementById("create-acct-btn");
 const returnBtn = document.getElementById("return-btn");
 
 var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
-
-function saveListsToFirebase(userId, watchlist, bookmarks, completedList) {
-  set(ref(database, 'users/' + userId), {
-    email: email,
-    watchlist: watchlist,
-    bookmarks: bookmarks,
-    completedList: completedList
-  });
-}
-
-// Retrieve the watchlist, bookmark, and completed-list from Firebase
-function retrieveListsFromFirebase(userId) {
-  onValue(ref(database, 'users/' + userId), (snapshot) => {
-    const user = snapshot.val();
-    if (user) {
-      const watchlist = user.watchlist || [];
-      const bookmarks = user.bookmarks || [];
-      const completedList = user.completedList || [];
-      localStorage.setItem("watchlist", JSON.stringify(watchlist));
-      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-      localStorage.setItem("completedList", JSON.stringify(completedList));
-    }
-  });
-}
-
-function handleUserLists(user) {
-  const userId = user.uid;
-  retrieveListsFromFirebase(userId); // retrieve lists from Firebase
-
-  // Listen for changes to the watchlist, bookmarks, and completed list in Firebase
-  onValue(ref(database, 'users/' + userId), (snapshot) => {
-    const user = snapshot.val();
-    if (user) {
-      const watchlist = user.watchlist || [];
-      const bookmarks = user.bookmarks || [];
-      const completedList = user.completedList || [];
-
-      // Load watchlist to watchlist.html
-      const watchlistHtml = document.getElementById("watchlist");
-      watchlistHtml.innerHTML = "";
-      watchlist.forEach((item) => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        watchlistHtml.appendChild(li);
-      });
-
-      // Load bookmarks to bookmark.html
-      const bookmarkHtml = document.getElementById("bookmark");
-      bookmarkHtml.innerHTML = "";
-      bookmarks.forEach((item) => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        bookmarkHtml.appendChild(li);
-      });
-
-      // Load completed list to complete.html
-      const completedHtml = document.getElementById("complete");
-      completedHtml.innerHTML = "";
-      completedList.forEach((item) => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        completedHtml.appendChild(li);
-      });
-    }
-  });
-}
 
 createacctbtn.addEventListener("click", function() {
   var isVerified = true;
@@ -124,13 +57,15 @@ createacctbtn.addEventListener("click", function() {
   }
   
   if(isVerified) {
-  createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    window.alert("Success! Account created.");
-    handleUserLists(user); // save or retrieve lists
-    window.location.href = "/"; // redirect to home page
-  })
+    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const userId = user.uid;
+        set(ref(database, 'users/' + userId), {
+          email: signupEmail
+        });
+        window.alert("Success! Account created.");
+      })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
@@ -143,20 +78,17 @@ submitButton.addEventListener("click", function() {
   email = emailInput.value;
   password = passwordInput.value;
 
- signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    console.log("Success! Welcome back!");
-    handleUserLists(user); // save or retrieve lists
-    window.location.href = "/"; // redirect to home page
-  })
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("Success! Welcome back!");
+    })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       window.alert("Error occurred. Try again.");
     });
 });
-
 
 signupButton.addEventListener("click", function() {
     main.style.display = "none";
