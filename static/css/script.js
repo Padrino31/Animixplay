fe4f918f57732e1ba3606997bd05f9ef9ef953d4
Padrino31 <92399279+Padrino31@users.noreply.github.com>
2023-04-09 15:06:22 -0600

    Update script.js

--- static/css/script.js
+++ static/css/script.js
@@ -35,6 +35,32 @@ const returnBtn = document.getElementById("return-btn");
 
 var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
 
+// Initialize Firebase Authentication
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in
+    const userId = user.uid;
+
+    // Get the data from localStorage
+    const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+    const completedList = JSON.parse(localStorage.getItem("completedList")) || [];
+
+    // Store the data in Firebase Realtime database
+    set(ref(database, `users/${userId}`), {
+      email: user.email,
+      watchlist,
+      bookmarks,
+      completedList,
+    }).then(() => {
+      console.log("Data saved to Firebase Realtime Database.");
+    }).catch((error) => {
+      console.error("Error occurred while saving data to Firebase Realtime Database:", error);
+    });
+  }
+});
+
 createacctbtn.addEventListener("click", function() {
   var isVerified = true;
 
@@ -61,89 +87,30 @@ createacctbtn.addEventListener("click", function() {
     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
       .then((userCredential) => {
         const user = userCredential.user;
-        const userId = user.uid;
-
-        // Get the data from localStorage
-        const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
-        const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
-        const completedList = JSON.parse(localStorage.getItem("completedList")) || [];
-
-        // Store the data in Firebase Realtime database
-        set(ref(database, `users/${userId}`), {
-          email: signupEmail,
-          watchlist,
-          bookmarks,
-          completedList,
-        }).then(() => {
-          window.alert("Success! Account created.");
-          window.location.href = "./login.html"; // redirect to homepage
-        }).catch((error) => {
-          window.alert("Error occurred while creating user. Try again.");
-        });
+        window.alert("Success! Account created.");
+        window.location.href = "./login.html"; // redirect to homepage
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+        console.error("Error occurred while creating user:", error);
         window.alert("Error occurred while creating user. Try again.");
       });
   }
 });
 
-// Save the data to Firebase Realtime Database when the page is refreshed or closed
-window.onbeforeunload = function() {
-  const user = auth.currentUser;
-  if (user) {
-    const userId = user.uid;
-    const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
-    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
-    const completedList = JSON.parse(localStorage.getItem("completedList")) || [];
-    
-    update(ref(database, `users/${userId}`), {
-      watchlist,
-      bookmarks,
-      completedList,
-    })
-    .then(() => {
-      console.log("Data saved to Firebase Realtime Database");
-    })
-    .catch((error) => {
-      console.error("Error occurred while saving data to Firebase Realtime Database:", error);
-    });
-  }
-};
-
-// Sign in and save the data to Firebase Realtime Database on button click
 submitButton.addEventListener("click", function() {
-  const email = emailInput.value;
-  const password = passwordInput.value;
+  email = emailInput.value;
+  password = passwordInput.value;
 
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-      console.log("Signed in as", user.email);
-      const userId = user.uid;
+      console.log("Success! Welcome back!");
 
-      // Get the data from Firebase Realtime Database
-      onValue(ref(database, `users/${userId}`), (snapshot) => {
-        const data = snapshot.val();
-        const watchlist = data.watchlist || [];
-        const bookmarks = data.bookmarks || [];
-        const completedList = data.completedList || [];
-
-        // Store the data in localStorage
-        localStorage.setItem("watchlist", JSON.stringify(watchlist));
-        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-        localStorage.setItem("completedList", JSON.stringify(completedList));
-
-        console.log("Data retrieved from Firebase Realtime Database");
-        window.location.href = "/"; // Redirect to homepage
-      }, {
-        onlyOnce: true // Listen for the value only once to avoid memory leaks
-      });
+      // Redirect to homepage after Firebase Authentication is successful
+      window.location.href = "/"; 
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
+      console.error("Error occurred while signing in:", error);
       window.alert("Error occurred. Try again.");
     });
 });
