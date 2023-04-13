65e3c28e2c5feca87ec1853f3937e6ecf5342e1e
Padrino31 <92399279+Padrino31@users.noreply.github.com>
2023-04-13 11:00:24 -0600

    Update serviceworker.js

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -10,3 +10,20 @@ workbox.routing.registerRoute(
   // eslint-disable-next-line no-undef
   new workbox.strategies.NetworkFirst()
 );
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    Notification.requestPermission().then(function(permission) {
+      if (permission === 'granted') {
+        return self.registration.showNotification('Animeflv', {
+          body: 'Do you want to receive notifications for new episodes?',
+          actions: [
+            {action: 'yes', title: 'Yes'},
+            {action: 'no', title: 'No'}
+          ]
+        });
+      }
+    })
+  );
+});
+
